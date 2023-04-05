5799f36bfbb2f9fad38cabe10ec348417ce383e6
HYPER <HYPER@HAYPER>
2023-04-05 22:17:32 +0400

    Добавьте файлы проекта.

---
+++ ExamlePlugin/Main.cs
@@ -0,0 +1,58 @@
+﻿using LSPD_First_Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rage;
+using Rage.Native;
+using LSPD_First_Response.Mod.API;
+using LSPD_First_Response.Mod.Callouts;
+using LSPD_First_Response.Engine.Scripting.Entities;
+using ExamplePlugin;
+using ExamplePlugin.Callouts;
+
+namespace ExamplePlugin
+{
+    public class Main : Plugin
+    {
+        //Initialization of the plugin.
+        public override void Initialize()
+        {
+            //This is saying that when our OnDuty status is changed, it calls for the code to call private static void OnOnDutyStateChangedHandler near the bottom of this page.
+            Functions.OnOnDutyStateChanged += OnOnDutyStateChangedHandler;
+
+            //Game.LogTrivial's are used to help you identify problems when debugging a crash with your plugin, so you know exactly what's going on when.
+
+            //This will show in the RagePluginHook.log as "Example Plugin 1.0.0.0 has been initialised." 
+            Game.LogTrivial("Example Plugin " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString() + " has been initialised.");
+
+            //This one will show in RagePluginHook.log as "Go on duty to fully load Example Plugin."
+            Game.LogTrivial("Go on duty to fully load Example Plugin.");
+        }
+        //This is a simple message saying that Example Plugin has been cleanup.
+        public override void Finally()
+        {
+            Game.LogTrivial("Example Plugin has been cleaned up.");
+        }
+        //This is called by Functions.OnOnDutyStateChanged as stated above, but only when bool OnDuty is true.
+        private static void OnOnDutyStateChangedHandler(bool OnDuty)
+        {
+            //We have to make sure they are actually on duty so the code can do its work, so we use an "if" statement.
+            if (OnDuty)
+            {
+                //This calls for the method private static void RegisterCallouts() down below.
+                RegisterCallouts();
+
+                //This shows a notification at the bottom left, above the minimap, of your screen when you go on duty, stating exactly what's in the quotation marks.
+                Game.DisplayNotification("Example Plugin v1.0 has loaded successfully, thank you for downloading!");
+            }
+        }
+        //This is the method that we called earlier in private static void OnOnDutyStateChangedHandler. This registers the callouts we have it setup to register, we'll come back to this after we make our callout.
+        private static void RegisterCallouts()
+        {
+            Functions.RegisterCallout(typeof(PursuitinProgress));
+        }
+
+    }
+
+ }
---
+++ ExamlePlugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ExamlePlugin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("LSPDFR:RU")]
+[assembly: AssemblyProduct("ExamlePlugin")]
+[assembly: AssemblyCopyright("Copyright LSPDFR:RU ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("a894392c-3ad1-45b7-8084-da4e03de8a9d")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
